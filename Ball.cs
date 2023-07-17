85173dff26d25e258efbf11c9d91e4fcf7a3394c
PetryckSlater <104164472+PetryckSlater@users.noreply.github.com>
2023-07-17 18:10:49 -0300

    Add files via upload

--- Ball.cs
+++ Ball.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-
-    public bool isRight;
+//o valor bool do isRight está sempre no false
+    public bool isRight; 
     public float speed;
     public Transform pointR;
     public Transform pointL;
@@ -27,8 +27,14 @@ public class Ball : MonoBehaviour
     }
     if(Input.GetMouseButton(0))
     {
-        isRight = !isRight;
-    } //Checando se esta precionando o botão esquerdo do mouse ( 0 = esquerdo) (1 = direito)
+        isRight = false; 
+    }
+    else
+    if(Input.GetMouseButton(1))
+    {
+        isRight = true;
+    }
+    //Checando se esta precionando o botão esquerdo do mouse ( 0 = esquerdo) (1 = direito)
     if(Vector2.Distance(transform.position, pointL.position) < 0.1 || Vector2.Distance(transform.position, pointR.position) < 0.1)
     {
         isRight = !isRight; //Para fazer a bola inverter
